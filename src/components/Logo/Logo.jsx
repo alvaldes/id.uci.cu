bc8885a201333509c078e23b033aded5421ab5f5
alvaldes <angelluis2605@gmail.com>
2021-09-27 17:21:23 -0400

    Ahora el logo lleva siempre a la pagina home

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import logo from '../../assets/images/logo.png'
 import clases from './Logo.css'
+import {Link} from 'react-router-dom';
+import {Box} from '@mui/material'
 
 const Logo = (props) => (
-    <div className={props.class}>
-            <img src={logo} alt='Logo' />
-        </div>
+    <div className={props.class} >
+        <Box component={Link} to='/'>
+            <img src={logo} alt='Logo'/>
+        </Box>
+    </div>
     );
 export default Logo;
